a2e9fe1e1f4fbbde65ee225bcd234dc22b5a4f5b
mayankbist45 <mayankbist45@gmail.com>
2019-02-07 11:31:05 +0530

    Adding SubSequence Generator

---
+++ Recursion/Subsequence_Generator.cpp
@@ -0,0 +1,27 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void subSequences(char *in,char* out,int i,int j){
+	if(in[i]=='\0'){
+		out[j]='\0';
+		cout<<out<<",";
+		return;
+	}
+
+	//Recurcive Case
+	//Include the current char
+	out[j] = in[i];
+	subSequences(in,out,i+1,j+1);
+	//Exclude the current char
+	subSequences(in,out,i+1,j);
+}
+
+
+int main() {
+	char input[100],output[100];
+	cin>>input;
+	subSequences(input,output,0,0);
+	return 0;
+}
