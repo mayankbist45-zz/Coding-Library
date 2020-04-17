d8c3c1958d1799b12cba63499dbc364008ff9cdb
Mayank Bist <mayankbist45@gmail.com>
2020-04-18 05:05:19 +0530

    Update trie.cpp

--- Trie/trie.cpp
+++ Trie/trie.cpp
@@ -1,82 +1,53 @@
-
 #include <bits/stdc++.h>
 
 using namespace std;
 
-#define hashmap unordered_map<char , node* > 
-
-class node{
-
-	public:
-	char data;
-	hashmap h;
-	bool is_terminal;
-
-	node(char d){
-		data = d;
-		is_terminal = false;
-	}
+class node {
+public:
+    map<char, node *> h;
+    bool is_terminal = false;
 };
 
-class Trie{
-	node *root;
-
+class Trie {
+    node *root;
 public:
-	Trie(){
-		root = new node('\0');
-	}
+    Trie() { root = new node(); }
 
-	//Insertion 
-	void addWord(char * word){
+    void addWord(string &word) {
+        node *temp = root;
+        for (char ch : word)
+            if (temp->h.count(ch) == 0) {
+                node *child = new node();
+                temp->h[ch] = child;
+                temp = child;
+            } else
+                temp = temp->h[ch];
+        temp->is_terminal = true;
+    }
 
-		node *temp = root;
-		for(int i=0;word[i]!='\0';i++){
-			 char ch = word[i];
-			 if(temp->h.count(ch)==0){
-			     node *child = new node(ch);
-			     temp->h[ch] = child;
-			     temp = child; 
-			 }
-			 else
-			 	temp = temp->h[ch];
-		}
-		temp->is_terminal = true;
-	}
-
-	//Lookup
-	bool isPresent(char*word){
-		node *temp = root;
-
-		for (int i = 0; word[i]!='\0' ;  ++i)
-		{
-			char ch = word[i];
-			if(temp->h.count(ch)){
-				temp = temp->h[ch];
-			}
-			else
-				return false;
-		}
-		return temp->is_terminal;
-	}
+    bool isPresent(string &word) {
+        node *temp = root;
+        for (char ch : word)
+            if (temp->h.count(ch))
+                temp = temp->h[ch];
+            else
+                return false;
+        return temp->is_terminal;
+    }
 };
 
 int main() {
+    vector<string> word = {"Mayank", "Superman", "Maya", "News", "Aman", "Ravi"};
+    Trie t;
+    for (auto &x:word)
+        t.addWord(x);
 
-	char word[][20 ] = {"Mayank","Superman","Maya","News","Aman","Ravi"};
-	Trie t;
+    string s;
+    cin >> s;
 
-	for (int i = 0; i < 6; ++i)
-	{		
-		t.addWord(word[i]);
-	}
-
-	char search[100];
-	cin.getline(search, 100);
-
-	if(t.isPresent(search)){
-		cout<<"Found";
-	}
-	else
-		cout<<"Not Found";
-	return 0;
+    if (t.isPresent(s)) {
+        cout << "Found";
+    } else
+        cout << "Not Found";
+    return 0;
 }
